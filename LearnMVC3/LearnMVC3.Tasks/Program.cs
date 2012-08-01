fd603b519af82ac832484f53eafcd5ec007ba4e2
Juan Huerta (Home) <juan.huerta@gmail.com>
2012-08-01 22:36:40 +0800

    Halfway in lesson 1st

---
+++ LearnMVC3/LearnMVC3.Tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnMVC3.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
