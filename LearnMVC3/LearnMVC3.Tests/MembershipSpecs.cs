37714ebe631c0e2dd4ca3b30840f30d9ed26e702
Juan Huerta <juan.huerta@gmail.com>
2012-08-03 17:32:19 +0800

    Start of Chapter 2

---
+++ LearnMVC3/LearnMVC3.Tests/MembershipSpecs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LearnMVC3.Tests
+{
+    [TestFixture]
+    public class MembershipSpecs : TestBase
+    {
+
+        [Test]
+        public void valid_email_and_passwords_should_register_user()
+        {
+            this.IsPending();
+        }
+
+    }
+
+}
---
+++ LearnMVC3/LearnMVC3.Tests/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LearnMVC3.Tests
+{
+    public class TestBase
+    {
+        public void Describes(string description)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(description);
+            Console.WriteLine("---------------------------");
+        }
+
+        public void IsPending()
+        {
+            Console.WriteLine(" {0} -- PENDING -- ", GetCaller());
+            Assert.Inconclusive();
+        }
+
+        public string GetCaller()
+        {
+            StackTrace stack = new StackTrace();
+            return stack.GetFrame(2).GetMethod().Name.Replace("_", " ");
+        }
+
+    }
+}
